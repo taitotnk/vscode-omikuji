34013d49ea015b450c56ac049f108de990dc62b1
Taito-Code <taito.tanaka1211@gmail.com>
2022-05-27 00:04:47 +0900

    Feature: omikuji function

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,27 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-omikuji" is now active!');
+  vscode.commands.registerCommand("vscode-omikuji.omikuji", () => {
+    const omikujiCandidates = [
+      "大吉",
+      "吉",
+      "中吉",
+      "小吉",
+      "末吉",
+      "凶",
+      "大凶",
+    ];
+    const omikujiResult =
+      omikujiCandidates[Math.floor(Math.random() * omikujiCandidates.length)];
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-omikuji.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-omikuji!');
-	});
-
-	context.subscriptions.push(disposable);
+    vscode.window.showInformationMessage(
+      `あなたの運勢は${omikujiResult}です！`
+    );
+  });
 }
 
 // this method is called when your extension is deactivated
